d32bb8f22d09297cc57f300d07e91fdd966b8996
Emily S <emily.sugano@gmail.com>
2018-07-30 21:06:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+ get "/newteam" do
+   erb :newteam
+ end
+
+ post "/newteam" do
+   @params = params.each do |key, value|
+              @key = key
+              @value = value
+            end 
+
+   erb :team
+ end
 
 
 end
